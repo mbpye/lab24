6a26dbb34a240e0d56d985519f36b73176eff97a
mbpye <73852907+mbpye@users.noreply.github.com>
2024-06-15 02:04:42 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,37 +1,72 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <locale.h>
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void simplify_fraction(char* expr) {
-    char* pos1, * pos2, * a, * b, * c;
+typedef struct Node {
+    char value[32];
+    struct Node* left;
+    struct Node* right;
+} Node;
 
-    if ((pos1 = strchr(expr, '/')) && (pos2 = strchr(pos1 + 1, '/'))) {
-        *pos1 = '\0';
-        a = expr;
-        b = pos1 + 1;
-        c = pos2 + 1;
-        printf("%s/(%s*%s)\n", a, b, c);
+
+Node* parseExpression(char* expr) {
+    Node* node = malloc(sizeof(Node));
+    strcpy(node->value, expr);
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+Node* convertToRPN(char* expr) {
+    return parseExpression(expr);
+}
+
+Node* buildExpressionTree(char* rpn) {
+    return convertToRPN(rpn);
+}
+
+void simplifyTree(Node* root) {
+    if (!root) return;
+    if (strcmp(root->value, "/") == 0 && root->right && strcmp(root->right->value, "/") == 0) {
+        strcpy(root->value, "*");
+        root->right = root->right->right;
     }
-    else if (pos1 && (pos2 = strchr(expr, '/'))) {
-        *pos2 = '\0';
-        a = expr;
-        b = pos2 + 1;
-        c = pos1 + 1;
-        printf("(%s/%s)*%s\n", a, b, c);
+    simplifyTree(root->left);
+    simplifyTree(root->right);
+}
+
+void traverseTree(Node* root, char* result) {
+    if (!root) return;
+    if (root->left) {
+        traverseTree(root->left, result);
     }
-    else {
-        printf("Expression format is incorrect.\n");
+    strcat(result, root->value);
+    if (root->right) {
+        traverseTree(root->right, result);
     }
 }
 
+void destroyTree(Node* root) {
+    if (!root) return;
+    destroyTree(root->left);
+    destroyTree(root->right);
+    free(root);
+}
+
 int main() {
-    setlocale(LC_ALL, "Rus");
     char expr[100];
-    printf("¬ведите сложную дробь: ");
-    scanf("%s", expr);
-    simplify_fraction(expr);
+    printf("Enter the expression: ");
+    scanf("%99s", expr);
+
+    Node* tree = buildExpressionTree(expr);
+    simplifyTree(tree);
+
+    char result[100] = { 0 };
+    traverseTree(tree, result);
+
+    printf("Simplified expression: %s\n", result);
+    destroyTree(tree);
+
     return 0;
 }
