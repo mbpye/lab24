e460af3c9fa2aea157c514763ed43359f530df03
mbpye <73852907+mbpye@users.noreply.github.com>
2024-06-14 23:32:01 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,110 +1,37 @@
 #define _CRT_SECURE_NO_WARNINGS
+#include <locale.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <locale.h>
 
+void simplify_fraction(char* expr) {
+    char* pos1, * pos2, * a, * b, * c;
 
-typedef struct Node {
-    char data[10];
-    struct Node* left;
-    struct Node* right;
-} Node;
-
-
-Node* createNode(char* data) {
-    Node* node = (Node*)malloc(sizeof(Node));
-    strcpy(node->data, data);
-    node->left = NULL;
-    node->right = NULL;
-    return node;
-}
-
-
-void printInfix(Node* root) {
-    if (root == NULL) return;
-    if (root->left != NULL) printf("(");
-    printInfix(root->left);
-    printf("%s", root->data);
-    printInfix(root->right);
-    if (root->right != NULL) printf(")");
-}
-
-
-Node* simplifyFraction(Node* root) {
-    if (root == NULL) return NULL;
-    if (strcmp(root->data, "/") == 0) {
-        if (root->left && strcmp(root->left->data, "/") == 0) {
-            Node* a = root->left->left;
-            Node* b = root->left->right;
-            Node* c = root->right;
-
-            Node* newRight = createNode("*");
-            newRight->left = b;
-            newRight->right = c;
-
-            Node* newRoot = createNode("/");
-            newRoot->left = a;
-            newRoot->right = newRight;
-
-            return newRoot;
-        }
-        if (root->right && strcmp(root->right->data, "/") == 0) {
-            Node* a = root->left;
-            Node* b = root->right->left;
-            Node* c = root->right->right;
-
-            Node* newLeft = createNode("/");
-            newLeft->left = a;
-            newLeft->right = b;
-
-            Node* newRoot = createNode("*");
-            newRoot->left = newLeft;
-            newRoot->right = c;
-
-            return newRoot;
-        }
+    if ((pos1 = strchr(expr, '/')) && (pos2 = strchr(pos1 + 1, '/'))) {
+        *pos1 = '\0';
+        a = expr;
+        b = pos1 + 1;
+        c = pos2 + 1;
+        printf("%s/(%s*%s)\n", a, b, c);
     }
-    root->left = simplifyFraction(root->left);
-    root->right = simplifyFraction(root->right);
-    return root;
+    else if (pos1 && (pos2 = strchr(expr, '/'))) {
+        *pos2 = '\0';
+        a = expr;
+        b = pos2 + 1;
+        c = pos1 + 1;
+        printf("(%s/%s)*%s\n", a, b, c);
+    }
+    else {
+        printf("Expression format is incorrect.\n");
+    }
 }
 
 int main() {
-    //1: (a/b)/c -> a/(b*c)
     setlocale(LC_ALL, "Rus");
-    Node* root1 = createNode("/");
-    root1->left = createNode("/");
-    root1->left->left = createNode("a");
-    root1->left->right = createNode("b");
-    root1->right = createNode("c");
-
-    printf("Исходное выражение 1: ");
-    printInfix(root1);
-    printf("\n");
-
-    root1 = simplifyFraction(root1);
-
-    printf("Преобразованное выражение 1: ");
-    printInfix(root1);
-    printf("\n\n");
-
-    //2: a/(b/c) -> (a/b)*c
-    Node* root2 = createNode("/");
-    root2->left = createNode("a");
-    root2->right = createNode("/");
-    root2->right->left = createNode("b");
-    root2->right->right = createNode("c");
-
-    printf("Исходное выражение 2: ");
-    printInfix(root2);
-    printf("\n");
-
-    root2 = simplifyFraction(root2);
-
-    printf("Преобразованное выражение 2: ");
-    printInfix(root2);
-    printf("\n");
-
+    char expr[100];
+    printf("Введите сложную дробь: ");
+    scanf("%s", expr);
+    simplify_fraction(expr);
     return 0;
 }
